2e9524e4b92eaacf5d9424fecfd047642ad0ca37
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-03-23 03:10:01 +0600

    Add Spinner component and show spinner while submitting form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationShcemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type FormData = z.infer<typeof createIssueSchema>;
 
@@ -25,6 +26,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   return (
     <div className='max-w-xl'>
@@ -37,9 +39,11 @@ const NewIssuePage = () => {
         className='space-y-3'
         onSubmit={handleSubmit(async (data) => {
           try {
+            setIsSubmitting(true);
             await axios.post('/api/issues', data);
             router.push('/issues');
           } catch (error) {
+            setIsSubmitting(false);
             setError('An error occurred, please try again');
           }
         })}
@@ -64,7 +68,13 @@ const NewIssuePage = () => {
 
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-        <Button>Submit new issue</Button>
+        {isSubmitting ? (
+          <Button disabled>
+            Submitting... <Spinner />
+          </Button>
+        ) : (
+          <Button type='submit'>Submit new issue</Button>
+        )}
       </form>
     </div>
   );
