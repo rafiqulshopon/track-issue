8472dbbf0c1e656e7514f52ba60ba62ab66d89f8
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-03-23 03:00:18 +0600

    Add ErrorMessage component and refactor error handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationShcemas';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type FormData = z.infer<typeof createIssueSchema>;
 
@@ -47,11 +48,7 @@ const NewIssuePage = () => {
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
 
-        {errors.title && (
-          <Text color='red' as='p'>
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
 
         <Controller
           name='description'
@@ -65,11 +62,7 @@ const NewIssuePage = () => {
           )}
         />
 
-        {errors.description && (
-          <Text color='red' as='p'>
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button>Submit new issue</Button>
       </form>
